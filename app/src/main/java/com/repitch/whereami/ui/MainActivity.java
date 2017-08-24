d84825a63755ad97a4ad13063acfbbc9671898c1
i.s.pyavkin <i.s.pyavkin@tinkoff.ru>
2017-08-24 10:11:06 +0300

    start/stop service show image state

--- app/src/main/java/com/repitch/whereami/ui/MainActivity.java
+++ app/src/main/java/com/repitch/whereami/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.repitch.whereami.ui;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.repitch.whereami.LocationService;
 import com.repitch.whereami.R;
@@ -31,15 +35,21 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TrackAdapter trackAdapter;
     private View empty;
+    private Button btnStartStopService;
+    private ImageView imgServiceState;
+
+    private boolean isStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_start_service).setOnClickListener(v -> checkPermissions());
+        btnStartStopService = findViewById(R.id.btn_start_service);
+        btnStartStopService.setOnClickListener(v -> startStopService());
         findViewById(R.id.btn_refresh).setOnClickListener(v -> refresh());
         recyclerView = findViewById(R.id.recycler);
         empty = findViewById(R.id.empty);
+        imgServiceState = findViewById(R.id.img_service_state);
 
         locationTrackRepository = new LocationTrackRepository();
         trackAdapter = new TrackAdapter(
@@ -53,11 +63,24 @@ public class MainActivity extends AppCompatActivity {
         } else {
             showContent();
         }
+
+        updateServiceState();
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void refresh() {
         List<LocationTrack> tracks = locationTrackRepository.getAll();
         trackAdapter.updateData(tracks);
+        updateServiceState();
     }
 
     private void showContent() {
@@ -70,28 +93,47 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setVisibility(View.GONE);
     }
 
-    private void checkPermissions() {
-        List<String> absentPermissions = new ArrayList<>();
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                absentPermissions.add(permission);
+    private void startStopService() {
+        if (isStarted) {
+            // need to stop
+            stopService(LocationService.createIntent(this));
+            isStarted = false;
+            setState(false);
+        } else {
+            List<String> absentPermissions = new ArrayList<>();
+            for (String permission : REQUIRED_PERMISSIONS) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    absentPermissions.add(permission);
+                }
+            }
+            if (absentPermissions.isEmpty()) {
+                startService(LocationService.createIntent(this));
+                isStarted = true;
+                setState(true);
+            } else {
+                String[] permissions = absentPermissions.toArray(new String[absentPermissions.size()]);
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        REQUEST_PERMISSIONS);
             }
         }
-        if (absentPermissions.isEmpty()) {
-            startService(LocationService.createIntent(this));
-        } else {
-            String[] permissions = absentPermissions.toArray(new String[absentPermissions.size()]);
-            ActivityCompat.requestPermissions(this,
-                    permissions,
-                    REQUEST_PERMISSIONS);
-        }
+    }
+
+    private void setState(boolean serviceStarted) {
+        btnStartStopService.setText(serviceStarted ? "остановить сервис" : "запустить сервис");
+        imgServiceState.setEnabled(serviceStarted);
+    }
+
+    private void updateServiceState() {
+        isStarted = isMyServiceRunning(LocationService.class);
+        setState(isStarted);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_PERMISSIONS:
-                checkPermissions();
+                startStopService();
                 return;
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
